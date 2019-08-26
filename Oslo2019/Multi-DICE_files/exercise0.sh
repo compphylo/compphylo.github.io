23c59a809d66b1427f886f1875956a9e1030b407
xanderxue <xanderxue@gmail.com>
2019-08-26 10:40:13 -0400

    Update exercise0.sh

--- Oslo2019/Multi-DICE_files/exercise0.sh
+++ Oslo2019/Multi-DICE_files/exercise0.sh
@@ -1,5 +1,15 @@
+#R CMD INSTALL MultiDICE_1.0.1.tar.gz
+
 unzip fsc26_linux64.zip
 
-R CMD INSTALL MultiDICE_1.0.1.tar.gz
+chmod +x fsc26_linux64/fsc26
+
+screen -S exercise1
+
+/usit/abel/u1/iovercast/miniconda3/bin/conda init
+
+bash
+
+conda activate Multi-DICE-dev
 
 R
