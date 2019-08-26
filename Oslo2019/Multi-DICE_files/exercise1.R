5d05ab22ea62235c0745d61ffd5d266fc5becbbd
xanderxue <xanderxue@gmail.com>
2019-08-26 10:40:44 -0400

    Update exercise1.R

--- Oslo2019/Multi-DICE_files/exercise1.R
+++ Oslo2019/Multi-DICE_files/exercise1.R
@@ -1,3 +1,3 @@
 library(MultiDICE)
 
-output = dice.sims(num.sims = 501, num.taxa = 1, num.partitions = 3, num.haploid.samples = 10, num.ind.sites = 1000, tau.psi.prior = 0, tau.idio.prior = c(10000:5000000), epsilon.idio.prior = list(c(500:2000)/10000, c(1), 10000/c(500:2000)), NE.idio.prior = list(c(10000:100000),c(1000:100000),c(1000:10000)), output.hyper.draws = F, output.taxa.draws = T, fsc2path = 'fsc26', output.directory = '.')
+output = dice.sims(num.sims = 501, num.taxa = 1, num.partitions = 3, num.haploid.samples = 10, num.ind.sites = 1000, tau.psi.prior = 0, tau.idio.prior = c(10000:5000000), epsilon.idio.prior = list(c(500:2000)/10000, c(1), 10000/c(500:2000)), NE.idio.prior = list(c(10000:100000),c(1000:100000),c(1000:10000)), output.hyper.draws = F, output.taxa.draws = T, fsc2path = 'fsc26_linux64/fsc26', output.directory = '.')
